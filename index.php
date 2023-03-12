02a10f638bfeabe848317ab9b1541c2094a5a9d6
TareqTech <125780248+tareqtech02@users.noreply.github.com>
2023-03-12 17:28:06 +0800

    my first page PHP
    my first page PHP

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to My Website</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h2>Home Page</h2>
+        <p>Welcome to my website! This is the home page.</p>
+        <p>Here's an example of some PHP code:</p>
+        <?php
+            $myName = "Tareq";
+            $course = "DevOps"
+            echo "Hello, my name is $myName!";
+
+            echo "Welcome in $course"
+        ?>
+    </main>
+    <footer>
+        <p>&copy; 2023 My Website</p>
+    </footer>
+</body>
+</html>
